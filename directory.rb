9248712d1e631b2b396f5318a1ee2e66d22ee984
Nicholas Leacock <nicholas@swissmail.com>
2017-01-31 15:29:02 +0000

    Subbed IF statements for array.

--- directory.rb
+++ directory.rb
@@ -5,68 +5,58 @@ def input_students
   students = []
   next_student, stdnt = "y", 0
 
+  arr = [[:name, "Which cohort?: "], [:cohort, "Enter hobbies: "], [:hobbies, "Enter favourite food: "],
+        [:favourite_food, "Enter country of birth: "], [:country, "Add another student? y : n ?"]]
+
   until next_student != "y"
     student = {}
     count = -1
 
-    puts "Please enter the name of the student"
+    puts first_line = "Please enter the name of the student"
     info = STDIN.noecho(&:gets).chomp
-    puts info.center(36)
 
     while count < 4 do
       count += 1
+      puts info.center(first_line.size - 2)
+      puts
+      puts (arr[count][1]).center(first_line.size)
+      student[(arr[count][0])] = info
 
-    if count == 0
-      puts "Which cohort?: ".center(36)
-      student[:name] = info
-    elsif count == 1
-      puts info.center(36)
-      puts "Enter hobbies: ".center(36)
-      student[:cohort] = info
-    elsif count == 2
-      puts info.center(36)
-      puts "Enter favourite food: ".center(36)
-      student[:hobbies] = info
-    elsif count == 3
-      puts info.center(36)
-      puts "Enter country of birth: ".center(36)
-      student[:favourite_food] = info
-    elsif count == 4
-      student[:country_of_birth] = info
+      if count == 4
+        next_student = STDIN.noecho(&:gets).chomp
+        puts next_student.center(36)
+      else
+        info = STDIN.noecho(&:gets).chomp
+      end
     end
-
-    if count == 4
-      puts info.center(36)
-      puts "Add another student? y : n ?".center(36)
-      next_student = STDIN.noecho(&:gets).chomp
-      puts next_student.center(36)
-    else
-      info = STDIN.noecho(&:gets).chomp
-    end
+    students << student
   end
-  students << student
-end
   students
 end
 
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts
+  puts "The students of Villains Academy".center(36)
+  puts "--------------------------------".center(36)
 end
 
 def print_students(students)
   count = 0
   while count != students.size
     student = students[count]
-    puts "#{count + 1}. #{student[:name]} from the #{student[:cohort]} cohort likes #{student[:hobbies]} and #{student[:favourite_food]} and was born in #{student[:country_of_birth]}"
+    puts "#{count + 1}. #{student[:name]} from the #{student[:cohort]} cohort".center(36)
+    puts "likes #{student[:hobbies]} and #{student[:favourite_food]}".center(36)
+    puts "and was born in #{student[:country]}".center(36)
     count += 1
+    puts
   end
 end
 
 def print_footer(students)
   students.size > 1 ? s = "s" : s = ""
-  puts "Overall, we have #{students.count} great student#{s}"
+  puts "Overall, we have #{students.count} great student#{s}".center(36)
+  puts
 end
 
 students = input_students
