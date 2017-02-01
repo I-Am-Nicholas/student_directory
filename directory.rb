aeeffe51faa177dabf2ae4e8ae7023efae3bccae
Nicholas Leacock <nicholas@swissmail.com>
2017-02-01 06:11:47 +0000

    Ex.7

--- directory.rb
+++ directory.rb
@@ -5,8 +5,8 @@ def input_students
   students = []
   next_student, stdnt = "y", 0
 
-  $arr = [[:name, "Which cohort?: "], [:cohort, "Enter hobbies: "], [:hobbies, "Enter favourite food: "],
-        [:favourite_food, "Enter country of birth: "], [:country, "Please check for errors."]]
+  $arr = [[:name, "Which cohort?:"], [:cohort, "Enter hobbies:"], [:hobbies, "Enter favourite food:"],
+        [:favourite_food, "Enter country of birth:"], [:country, "Please check for errors."]]
 
   until next_student == "n"
     student = {}
@@ -14,54 +14,69 @@ def input_students
     err_input = 0
     puts
     puts first_line = "Please enter the name of the student"
+    puts
     info = STDIN.noecho(&:gets).chomp
+    $l = first_line.size
 
     while count < $arr.size - 1 do
       info = "[No input]" if info == ""
       count += 1
-      puts ("#{count + 1}) #{info.center(first_line.size - 2)}").center(36)
+      puts ("#{count +1}. " + info).center($l)
       puts
-      puts ($arr[count][1]).center(first_line.size)
+      puts ($arr[count][1]).center($l)
       student[($arr[count][0])] = info
-
       error = "y"
+
       if count == $arr.size - 1
         until error == "n"
-          puts "To corrct an entry just enter".center(36)
-          puts "the corresponding number".center(36)
-          puts "To exit this option, ENTER.".center(36)
+          puts "To corrct an entry just enter".center($l)
+          puts "the corresponding number".center($l)
+          puts "To exit this option, press ENTER.".center($l)
+          puts
           error_num = STDIN.noecho(&:gets).chomp
-
           error_num == "" ? break : error_num = error_num.to_i
 
-          while !(1..(($arr.size) -1)).include?(error_num)
-            puts "Invalid entry. Try again.".center(36)
-            error_num = STDIN.noecho(&:gets).chomp.to_i
-            break if error_num == ""
+          while !(1..$arr.size).include?(error_num)
+            puts "Invalid entry. Try again.".center($l)
+            error_num = STDIN.noecho(&:gets).chomp
+            error_num == "" ? break : error_num = error_num.to_i
           end
+
+          break if error_num == ""
 
           wrong = student[($arr[error_num -1][0])]
           puts
-          puts "Changing: #{wrong}".center(36)
+          puts "Changing: #{wrong}".center($l)
           right = STDIN.noecho(&:gets).chomp
           student[($arr[error_num - 1][0])] = right
-          puts "Changed #{wrong} to #{right}.".center(36)
-          puts "Any more errors? y : n".center(36)
-          error = gets.chomp
-
-
-# implant error block here
-
-
-
+          puts "Changed #{wrong} to #{right}.".center($l)
+          puts
+          puts "Any more errors? y : n".center($l)
+          error = STDIN.noecho(&:gets).chomp
+          puts error.center($l)
+          puts
+          until error == "y" || error == "n"
+            puts "Invalid entry. Try again.".center($l)
+            error = STDIN.noecho(&:gets).chomp
+            puts error.center($l)
+          end
         end
       end
+
       info = STDIN.noecho(&:gets).chomp if count < $arr.size - 1
       if count == $arr.size - 1
         puts
-        puts "Add another student? y : n ?".center(36)
+        puts "Add another student? y : n ?".center($l)
         next_student = STDIN.noecho(&:gets).chomp
-        puts next_student.center(36)
+
+
+        until next_student == "y" || next_student == "n"
+          puts "Invalid entry. Try again.".center($l)
+          next_student = STDIN.noecho(&:gets).chomp
+        end
+
+
+        puts next_student.center($l)
       end
     end
     students << student
@@ -72,18 +87,18 @@ end
 
 def print_header
   puts
-  puts "The students of Villains Academy".center(36)
-  puts "--------------------------------".center(36)
+  puts "The students of Villains Academy".center($l)
+  puts "--------------------------------".center($l)
 end
 
 def print_students(students)
   count = 0
   while count != students.size
     student = students[count]
-    puts "(#{count +1})".center(36)
+    puts "(#{count +1})".center($l)
     x = 0
     until x == $arr.size
-      puts "#{($arr[x][0]).to_s}: #{student[($arr[x][0])]}".center(36)
+      puts "#{($arr[x][0]).to_s}: #{student[($arr[x][0])]}".center($l)
       x += 1
     end
     count += 1
@@ -93,7 +108,7 @@ end
 
 def print_footer(students)
   students.size > 1 ? s = "s" : s = ""
-  puts "Overall, we have #{students.count} great student#{s}".center(36)
+  puts "Overall, we have #{students.count} great student#{s}".center($l)
   puts
 end
 
