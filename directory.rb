1c58bd7eaa8c4099e16f8e661608567ef9835955
Nicholas Leacock <nicholas@swissmail.com>
2017-02-03 18:51:59 +0000

    Ex 12 Loading from file.

--- directory.rb
+++ directory.rb
@@ -20,6 +20,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       puts
       exit
@@ -29,12 +31,12 @@ def process(selection)
 end
 
 
-
 def print_menu
     puts
     puts "1. Input the students".center($l)
     puts "2. Show the students".center($l)
     puts "3. Save the list to students CSV.".center($l)
+    puts "4. Load the list from students CSV.".center($l)
     puts "9. Exit".center($l)
     puts
 end
@@ -44,6 +46,16 @@ def show_students
   print_header
   print_students_list
   print_footer
+end
+
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 
